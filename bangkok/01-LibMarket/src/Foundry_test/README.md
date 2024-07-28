700abd422b95a29c3d7fd245f4ca81f01d6b9262
S7iter <115307551@qq.com>
2024-07-28 18:36:51 +0800

    add foundry test

--- bangkok/01-LibMarket/src/Foundry_test/README.md
+++ bangkok/01-LibMarket/src/Foundry_test/README.md
@@ -1,3 +1,12 @@
+
+## Introduction
+
+
+Use foundry to test contract GAS and code security
+
+
+
+
 ## Foundry
 
 **Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
--- bangkok/01-LibMarket/src/Foundry_test/script/Counter.s.sol
+++ bangkok/01-LibMarket/src/Foundry_test/script/Counter.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract C2CPlatform is Script {
     function setUp() public {}
 
     function run() public {
