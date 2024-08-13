915a56308e88bf8d015c2e1bdb758882d1d71d88
工藤权一 <115307551@qq.com>
2024-08-13 16:47:21 +0800

    Ownable2Step

--- bangkok/01-LibMarket/src/contracts/Owner_Pausable.sol
+++ bangkok/01-LibMarket/src/contracts/Owner_Pausable.sol
@@ -1,31 +1,36 @@
 // SPDX-License-Identifier: LibMarket
 pragma solidity ^0.8.20;
 
-// 定义owner 用来暂停或者开放合约
-// 用于整个系统的开放和关闭
-contract Ownable {
-    address public owner;
+import "./Ownable2Step.sol" 
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+// contract Ownable {
+//     address public owner;
 
-    constructor() {
-        owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
-    }
+//     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Ownable: caller is not the owner");
-        _;
-    }
+//     constructor() {
+//         owner = msg.sender;
+//         emit OwnershipTransferred(address(0), msg.sender);
+//     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-}
+//     modifier onlyOwner() {
+//         require(owner == msg.sender, "Ownable: caller is not the owner");
+//         _;
+//     }
 
-contract Pausable is Ownable {
+//     function transferOwnership(address newOwner) public onlyOwner {
+//         require(newOwner != address(0), "Ownable: new owner is the zero address");
+//         emit OwnershipTransferred(owner, newOwner);
+//         owner = newOwner;
+//     }
+// }
+
+
+/**
+ * 定义owner 用来暂停或者开放合约用于整个系统的开放和关闭
+ */
+
+contract Pausable is Ownable2Step {
     bool public paused;
 
     event Paused(address account);
