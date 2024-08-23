05a3acccb0bc72418a22fcc03ca188b4dc6afb8c
Teslim Ibisomi <ibisomimayowa@gmail.com>
2024-08-23 09:39:54 +0100

    create ticket page with crust storage API implemented

--- singapore/BlockPass/src/ui/src/AppRoutes.jsx
+++ singapore/BlockPass/src/ui/src/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Index from "./pages/landingPage/index";
-// import Event from "./pages/CreateEvent/CreateEvent";
+import Event from "./pages/CreateEvent/CreateEvent";
 // import Tickets from "./pages/MyTicket/MyTickets";
 // import Gallery from "./pages/EventGallery/EventGallery"; To be implemented later or add some later
 
@@ -9,8 +9,9 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
-      {/* <Route path="create" element={<Event />} />
-      <Route path="my-tickets" element={<Tickets />} />
+      <Route path="create" element={<Event />} />
+      
+      {/*<Route path="my-tickets" element={<Tickets />} />
       <Route path="gallery" element={<Gallery />} /> */}
     </Routes>
   );
