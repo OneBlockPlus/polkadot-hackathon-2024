c4745c831648794c238d00354de6d6a513367b52
Jakub Vonšovský <jakub.vonsovsky@gmail.com>
2024-10-17 19:06:42 +0200

    Add database and AJAX loading of records

--- bangkok/39-GenomTex/app.py
+++ bangkok/39-GenomTex/app.py
@@ -1,11 +1,44 @@
 from flask import Flask, render_template
+from db_model import DBModel
+
+from datetime import datetime
 
 app = Flask(__name__)
+model = DBModel("api_storage_module/genomtex.sqlite")
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    model.close()
 
 
 @app.route('/')
 def index():  # put application's code here
     return render_template('index.html')
+
+
+@app.route('/record/<string:name>')
+def get_record(name: str):
+    """
+    Get client's records by his name
+    """
+
+    records = model.get_client_records_by_name(name)
+    client = None
+    if len(records) > 0:
+        client = {
+            "id": records[0]["id"],
+            "name": records[0]["name"],
+            "dna_fingerprint": records[0]["dna_fingerprint"],
+        }
+    records = [{
+        "sequenced_at": record["sequenced_at"].split(" ")[0],
+        "full_file_name": record["full_file_name"],
+        "diff_file_name": record["diff_file_name"],
+        "ipfs_cid": record["ipfs_cid"],
+    } for record in records]
+
+    return render_template('record.html', name=name, client=client, records=records)
 
 
 if __name__ == '__main__':
---
+++ bangkok/39-GenomTex/db_model.py
@@ -0,0 +1,56 @@
+import logging
+import sqlite3
+from typing import Any, List, Tuple, Optional
+
+logger = logging.getLogger(__name__)
+
+
+class DBModel:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.connection = None
+
+    def get_connection(self) -> sqlite3.Connection:
+        if self.connection is None:
+            self.connection = sqlite3.connect(self.filepath)
+            self.connection.row_factory = sqlite3.Row
+        return self.connection
+
+    def commit(self):
+        self.get_connection().commit()
+
+    def close(self):
+        self.get_connection().close()
+        self.connection = None
+
+    def select_value(self, cur, sql: str, sql_params: Tuple[Any] = ()):
+        try:
+            return cur.execute(sql, sql_params).fetchone()
+        except sqlite3.DatabaseError as error:
+            logger.error(error)
+
+    def get_record_by_id(self, table: str, rec_id: int, columns: List[str] = []):
+        cur = self.get_connection().cursor()
+        col_sel = '","'.join(columns)
+        if len(col_sel) == 0:
+            col_sel = "*"
+        else:
+            col_sel = f'"{col_sel}"'
+
+        query = "SELECT {} FROM {} WHERE id = %s".format(col_sel, table)
+
+        return self.select_value(cur=cur, sql=query, sql_params=(rec_id,))
+
+    def get_client_records_by_name(self, name) -> List[sqlite3.Row]:
+        cur = self.get_connection().cursor()
+
+        query = "SELECT C.id, C.name, C.dna_fingerprint, G.sequenced_at, " \
+                "G.full_file_name, G.diff_file_name, G.ipfs_cid " \
+                "FROM clients C " \
+                "LEFT JOIN genomes G ON G.client_id = C.id " \
+                "WHERE C.name = ? COLLATE NOCASE " \
+                "ORDER BY G.sequenced_at"
+
+        result = cur.execute(query, (name,))
+        return result.fetchall()
