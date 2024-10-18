9a018fdef60e6e85c42068e74b7c4044b9d0582a
Jakub Vonšovský <jakub.vonsovsky@gmail.com>
2024-10-18 16:58:38 +0200

    Code for diffing files

--- bangkok/39-GenomTex/api_storage_module/genomics/strings.py
+++ bangkok/39-GenomTex/api_storage_module/genomics/strings.py
@@ -1,47 +1,61 @@
-import difflib
 import logging
-from time import time
-from typing import List
+import os
+import shutil
+import subprocess
+from typing import List, Tuple
 
 logger = logging.getLogger(__name__)
 
 
-def make_patch_from_files(filepath1: str, filepath2: str, seq_num: int) -> str:
+def make_patch_from_files(filepath1: str, filepath2: str) -> Tuple[str, str]:
     """
     Use this function to store differences between two .vcf files,
     which are already differentials to the reference genomic sequence.
 
+    Example: make_patch_from_files("8f0fd05f.vcf", "8f0fd05f.v2.vcf")
+
     :return: Stored diff filename
     """
 
-    with open(filepath1, "r") as fr:
-        dna1 = fr.readlines()
+    if not os.path.exists(filepath1):
+        raise FileNotFoundError(filepath1)
+    if not os.path.exists(filepath2):
+        raise FileNotFoundError(filepath2)
 
-    with open(filepath2, "r") as fr:
-        dna2 = fr.readlines()
+    basename = ".".join(filepath2.split(".")[:-1])
+    outfilename = f"{basename}.diff"
+    with open(outfilename, 'w') as file:
+        p = subprocess.run(['diff', filepath1, filepath2], stdout=file)
 
-    start = time()
-    diff = difflib.unified_diff(dna1, dna2, lineterm='')
-    logger.info(f"Finished in {time() - start} seconds")
+    err = None
+    if p.returncode == 2:
+        err = p.stderr.decode()
 
-    basename = filepath1.split(".")[0]
-    outfilename = f"{basename}.{seq_num}.diff"
-    with open(outfilename, 'w') as fw:
-        fw.write('\n'.join(diff))
-
-    return outfilename
+    return outfilename, err
 
 
-def apply_patch_to_file(filepath_src: str, filepath_diff: str) -> str:
+def apply_patch_to_file(filepath_src: str, filepath_diff: str) -> Tuple[str, str]:
     """
     Use this function to apply a patch to .vcf file to reconstruct its sequential version.
 
     :return: Reconstructed sequential .vcf file.
     """
 
-    # TODO
+    if not os.path.exists(filepath_src):
+        raise FileNotFoundError(filepath_src)
+    if not os.path.exists(filepath_diff):
+        raise FileNotFoundError(filepath_diff)
 
-    pass
+    basename = ".".join(filepath_diff.split(".")[:-1])
+    outfilename = f"{basename}.vcf"
+    shutil.copyfile(filepath_src, outfilename)
+    p = subprocess.run(['patch', outfilename, filepath_diff], capture_output=True)
+
+    err = None
+    if p.returncode != 0:
+        err = p.stderr.decode()
+
+    return outfilename, err
 
 
 def reconstruct_dna_from_diff(ref_genome_f: str, diff_genome_f: str) -> List[str]:
@@ -54,3 +68,24 @@ def reconstruct_dna_from_diff(ref_genome_f: str, diff_genome_f: str) -> List[str
     # TODO
 
     pass
+
+
+def test_make_and_apply_patch_from_files():
+    os.chdir('/Users/Kuba/Projects/polkadot-hackathon-2024/bangkok/39-GenomTex/api_storage_module/client_data')
+    diff_filename, err = make_patch_from_files("8f0fd05f.vcf", "8f0fd05f.v2.vcf")
+    assert err is None
+    shutil.move("8f0fd05f.v2.vcf", "_8f0fd05f.v2.vcf")
+    patched_filename, err = apply_patch_to_file("8f0fd05f.vcf", diff_filename)
+    assert patched_filename == "8f0fd05f.v2.vcf"
+    assert err is None
+
+    # no diff between original v2 and patched v1 -> v2 file
+    diff_filename, err = make_patch_from_files("_8f0fd05f.v2.vcf", "8f0fd05f.v2.vcf")
+    stats = os.stat(diff_filename)
+    assert stats.st_size == 0
+    assert err is None
+    os.remove(diff_filename)
+
+
+if __name__ == "__main__":
+    test_make_and_apply_patch_from_files()
