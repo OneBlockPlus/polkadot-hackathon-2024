280a37b062a2c10398b6a2704d51e2067a0e0518
Wario <69741055+wariomx@users.noreply.github.com>
2024-10-22 22:28:48 +0700

    softlaw_submit

---
+++ bangkok/36-SoftLaw/src/IP_pallet/src/tests.rs
@@ -0,0 +1,8 @@
+mod escrow_nft_tests;
+mod mint_nft_tests;
+mod create_license_tests;
+mod accept_license_tests;
+mod revoke_license_tests;
+mod make_periodic_payment_tests;
+mod on_initialize_tests;
+
---
+++ bangkok/36-SoftLaw/src/ui/src/components/landing/hero.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { TextSpan } from "@/components/TextSpan";
+
+
+function Hero() {
+const ricardian = "RICARDIAN CONTRACTS".split("");
+const legalChains = "LEGAL CHAINS".split("");
+const realWorldAssets = "REAL WORLD ASSETS".split("");
+
+    return (
+        <div>
+             
+      <div
+        className="relative bg-[url(/images/Container1.svg)] bg-no-repeat bg-cover bg-center w-full h-screen flex justify-center items-center p-[25px] "
+      >
+       <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#1C1A11]"></div>
+
+        <div className="pt-[20px] z-10 ">
+          <img src="/images/logoHero.svg" className="w-3/4 mx-auto my-auto lg:w-1/2 min-[2000px]:w-full " />
+          <div className="flex items-center flex-col md:flex-row gap-6 mt-10 mb-10 text-yellow-400 justify-center min-[2000px]:text-4xl">
+            <div className="md:text-2xl xl:text-3xl flex">
+              {ricardian.map((letter, index) => (
+                <TextSpan key={index}>
+                  {letter === " " ? "\u00A0" : letter}
+                </TextSpan>
+              ))}
+            </div>
+
+            <div className="md:text-2xl xl:text-3xl flex min-[2000px]:text-4xl">
+              {legalChains.map((letter, index) => (
+                <TextSpan key={index}>
+                  {letter === " " ? "\u00A0" : letter}
+                </TextSpan>
+              ))}
+            </div>
+            <div className="sm:text-lg md:text-2xl xl:text-3xl flex min-[2000px]:text-4xl">
+              {realWorldAssets.map((letter, index) => (
+                <TextSpan key={index}>
+                  {letter === " " ? "\u00A0" : letter}
+                </TextSpan>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* </section> */}
+      </div>
+      
+        </div>
+    )
+}
+
+export default Hero;
